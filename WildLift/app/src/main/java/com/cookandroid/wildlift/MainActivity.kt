769c43443ccc8cf0821e7ccb8d7d96c2544fb1e6
yuyw0712 <yuyw0712@naver.com>
2020-11-06 02:58:50 +0900

    메인화면 네비게이션드로어 + 챔피언,아이템,룬 액티비티 구현

--- WildLift/app/src/main/java/com/cookandroid/wildlift/MainActivity.kt
+++ WildLift/app/src/main/java/com/cookandroid/wildlift/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.cookandroid.wildlift
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        // 왼쪽 상단에 네비게이션바 만들기
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        //아이템 선택 이벤트 호출
+        navigationView.setNavigationItemSelectedListener(this)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.championInformation -> {
+                startActivity(Intent(this, ChampionActivity::class.java))
+            }
+            R.id.itemInformation -> {
+                startActivity(Intent(this, ItemActivity::class.java))
+            }
+            R.id.runesSpell -> {
+                startActivity(Intent(this, RunesActivity::class.java))
+            }
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
