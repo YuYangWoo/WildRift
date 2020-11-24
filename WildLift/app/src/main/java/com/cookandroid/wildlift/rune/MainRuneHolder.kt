ca945213141667977bd2ea8a76a3cc14c87e7430
KangTaeJong98 <rkdxowhd98@naver.com>
2020-11-24 15:42:54 +0900

    Rune Activity 구현완료

---
+++ WildLift/app/src/main/java/com/cookandroid/wildlift/rune/MainRuneHolder.kt
@@ -0,0 +1,7 @@
+package com.cookandroid.wildlift.rune
+
+import com.cookandroid.wildlift.base.BaseHolder
+import com.cookandroid.wildlift.databinding.HolderMainRuneBinding
+
+class MainRuneHolder(binding: HolderMainRuneBinding) : BaseHolder<HolderMainRuneBinding, Rune>(binding) {
+}
--- WildLift/app/src/main/java/com/cookandroid/wildlift/rune/RuneHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.cookandroid.wildlift.rune
-
-import com.cookandroid.wildlift.base.BaseHolder
-import com.cookandroid.wildlift.databinding.HolderRuneBinding
-
-class RuneHolder(binding: HolderRuneBinding) : BaseHolder<HolderRuneBinding, Rune>(binding) {
-}
