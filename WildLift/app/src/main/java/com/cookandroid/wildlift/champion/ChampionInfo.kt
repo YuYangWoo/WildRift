b7827c31ace459b1e142570ec233823983148395
yuyw0712 <yuyw0712@naver.com>
2020-11-25 17:00:30 +0900

    스킬,스킨 / 챔피언 유니버스 웹뷰 구현 완료

--- WildLift/app/src/main/java/com/cookandroid/wildlift/champion/ChampionInfo.kt
+++ WildLift/app/src/main/java/com/cookandroid/wildlift/champion/ChampionInfo.kt
@@ -1,9 +1,13 @@
 package com.cookandroid.wildlift.champion
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.cookandroid.wildlift.R
+import com.cookandroid.wildlift.champion.championInfo.ChampionSkillSkin
+import com.cookandroid.wildlift.champion.championInfo.ChampionUniverse
 import kotlinx.android.synthetic.main.activity_champion_info.*
 
 class ChampionInfo : AppCompatActivity() {
@@ -17,6 +21,7 @@ class ChampionInfo : AppCompatActivity() {
         var champImg = intent.getStringExtra("championImage")
         var champName = intent.getStringExtra("championName")
         var champPosition = intent.getStringExtra("championPosition")
+        var champEngName = intent.getStringExtra("championEngName")
 
         Glide.with(this) // 띄어줄 뷰를 명시
             .load(champImg) // 이미지 주소
@@ -26,8 +31,15 @@ class ChampionInfo : AppCompatActivity() {
 
         // 스킬/스킨 버튼
         btnSkillSkin.setOnClickListener {
-
+            var intent = Intent(this,ChampionSkillSkin::class.java)
+            intent.putExtra("championEngName", champEngName)
+            startActivity(intent)
         }
         // 챔피언 유니버스 버튼
+        btnUniverse.setOnClickListener {
+            var intent = Intent(this,ChampionUniverse::class.java)
+            intent.putExtra("championEngName", champEngName)
+            startActivity(intent)
+        }
     }
 }
