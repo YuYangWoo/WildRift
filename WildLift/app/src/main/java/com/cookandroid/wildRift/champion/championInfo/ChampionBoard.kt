dd329b1ff26bec26522e775cdb41a33f6ebd2499
yuyw0712 <yuyw0712@naver.com>
2020-12-18 23:16:46 +0900

    yangwoo - 챔피언 한 줄 게시판 기초 구현

---
+++ WildLift/app/src/main/java/com/cookandroid/wildRift/champion/championInfo/ChampionBoard.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.wildRift.champion.championInfo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cookandroid.wildRift.R
+
+class ChampionBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_champion_board)
+    }
+}
